11b970ea87a8e48477916b3e3cd34a51d31cc5d3
Nenad Stokic <nstokic@gmail.com>
2020-03-25 22:44:22 +0100

    -Server side validation with express-validator

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const feedController = require("../controllers/feed");
 const router = express.Router();
+const { body } = require("express-validator");
 
 // GET /feed/posts
 router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 7 }),
+    body("content")
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
