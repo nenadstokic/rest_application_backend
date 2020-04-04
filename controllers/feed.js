9e0f0233119a77f0654ffe87fc3fa2b982171b0a
Nenad <nstokic@gmail.com>
2020-04-05 01:00:31 +0200

    Deleting a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -120,7 +120,32 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find the post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      // chec logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Deleted post.' });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
 const clearImage = filePath => {
-  filepath = join(__dirname, '..', filepath);
-  fs.unlink(filepath, err => console.log(err));
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
